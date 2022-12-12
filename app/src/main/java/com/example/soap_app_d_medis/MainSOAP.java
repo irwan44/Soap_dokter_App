9719e0730fca97509573f5aa66bda4b9a3428052
musa_nur_rahman <musa48family@gmail.com>
2022-12-12 10:09:19 +0700

    musa awal pagi

--- app/src/main/java/com/example/soap_app_d_medis/MainSOAP.java
+++ app/src/main/java/com/example/soap_app_d_medis/MainSOAP.java
@@ -7,20 +7,30 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 public class MainSOAP extends AppCompatActivity {
+
+    String val_token, soap_selesai, soap_pending, soap_total;
+    TextView txt_soap_selesai, txt_soap_pending, txt_soap_total;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_soap);
-
         getSupportActionBar().hide();
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S){
             getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         }
 
+        txt_soap_selesai = findViewById(R.id.txt_soap_selesai);
+        txt_soap_pending = findViewById(R.id.txt_soap_pending);
+        txt_soap_total = findViewById(R.id.txt_soap_total);
+
+    }
+
+    private void getDataSoap(){
+        
     }
 
     public void soapmenu (View view) {
